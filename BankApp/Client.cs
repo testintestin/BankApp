39c60d7083d55ff34ff2a9c38476a3ab1b00286b
testintestin <jasper.smet2@telenet.be>
2017-01-03 14:09:20 +0100

    test

--- BankApp/Client.cs
+++ BankApp/Client.cs
@@ -10,7 +10,7 @@ namespace BankApp
     [Serializable]
     public class Client : IEnumerable
     {
-        // Medlemsvariabler
+        // Medlemsvariabler 
         public int id { get; set; }
         public string first_name { get; set; }
         public string last_name { get; set; }
